68d21f8372980f8e6b66780d7fdeb661378d381f
“skpai” <suresh.sasidharan-pillai@le-campus-numerique.fr>
2021-03-31 09:25:40 +0200

    initial

---
+++ oop.py
@@ -0,0 +1,8 @@
+class animal: # This line starts the definition of a class
+    def __init__(self, type): # This a constructor function
+        type=""        
+    def __str__(self):
+        return  type
+    def se_deplacer(self):
+        return None
+    
