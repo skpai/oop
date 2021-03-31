1933f255334fb92bf452b4fbc87de4a86df73826
“skpai” <suresh.sasidharan-pillai@le-campus-numerique.fr>
2021-03-31 10:47:53 +0200

    working

--- oop.py
+++ oop.py
@@ -1,8 +1,58 @@
-class animal: # This line starts the definition of a class
-    def __init__(self, type): # This a constructor function
-        type=""        
+class Zoo : 
+    def __init__(self, liste): 
+        self.liste=liste 
     def __str__(self):
-        return  type
+        return ''.join([l.animal_type+" " for l in self.liste])
+
+    def add_animal(self, a):
+        return self.liste.append(a)
+
+    def dosomething(self):
+        for l in self.liste:
+            print(l.dosomething(), end=",")
+
+
+class Animal: 
+    def __init__(self, animal_type): 
+        self.animal_type=animal_type 
+    
+    def __str__(self):
+        return self.animal_type
     def se_deplacer(self):
+        print("none")
         return None
     
+    def dosomething(self):
+        print("something")
+
+class Serpent (Animal):  
+    def __str__(self):
+        return self.animal_type          
+    def se_deplacer(self):
+        print("je rampe")
+        return None
+
+class Oiseau(Animal):
+    def __str__(self):
+        return self.animal_type         
+    def se_deplacer(self):
+        print("je vole")
+    def altitude_max(self):
+        return 100
+    def dosomething(self):
+        super().dosomething() 
+
+a = Animal('cat')
+a.se_deplacer()
+s=Serpent("Serpent")
+s.se_deplacer()
+o=Oiseau("Oiseau")
+o.se_deplacer()
+o.dosomething()
+print(o.altitude_max())
+z=Zoo([s,o])
+z.dosomething()
+print(z)
+z.add_animal(a)
+print(z)
+
