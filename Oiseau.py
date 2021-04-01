0a67b6826cba0a224437a369085da92510673467
skpai <suresh.sasidharan-pillai@le-campus-numerique.fr>
2021-04-01 09:26:23 +0100

    getter and setter added to bird class

--- Oiseau.py
+++ Oiseau.py
@@ -1,11 +1,21 @@
 from Animal import Animal
 class Oiseau(Animal):
-    _animal_type="Oiseau"
+    def __init__(self, animal_type, weight, height,_altitude_max ):
+        super().__init__(animal_type, weight, height) 
+        self._altitude_max =_altitude_max
+        self._animal_type="Oiseau"
+    @property
+    def altmax(self):
+        return self._altitude_max
+    @altmax.setter #For no use now
+    def altmax(self, value):
+        print("setter of altitude_max called")
+        self._altitude_max = value
+
+
     def __str__(self):
         return self.animal_type         
     def se_deplacer(self):
         print("je vole")
-    def altitude_max(self):
-        return 100
     def dosomething(self):
         super().dosomething() 
